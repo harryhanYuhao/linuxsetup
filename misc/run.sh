65ccc6030c466064745d896d025a405ff19ab334
Yuhao Harry Han <s2163783@ed.ac.uk>
2023-10-02 23:03:30 +0100

    run script

--- misc/run.sh
+++ misc/run.sh
@@ -1,19 +1,48 @@
 #!/bin/sh
 
+set -e
+
 help(){
 	echo "run.sh: install current config to system, or update current file with system configs"
 	echo 
-	echo "syntax: run.sh [-h|i|u|p]"
+	echo "syntax: run.sh -[h|i|u|p]"
 	echo "Options:"
-	echo "h		show this help message"
-	echo "i		install the configuration in this folder to system"
-	echo "u		update files in this folder with system configuration"
+	echo "-h		show this help message"
+	echo "-i		install the configuration in this folder to system"
+	echo "-u		update files in this folder with system configuration"
+}
+
+install(){
+	echo todo!
+}
+
+update(){
+	cp -r ~/.bashrc.d -t .
+	mv .bashrc.d bashrc.d
+	cp -r ~/.bashrc.cmd.d -t .
+	mv .bashrc.cmd.d bashrc.cmd.d
+	cp ~/.bashrc -t .
+	mv .bashrc bashrc
 }
 
 while getopts ":hiup" option; do
 	case $option in
 		h)
-			hello
+			help
+			exit 0
+			;;
+		i)
+			install
+			exit 0
+			;;
+		u)
+			update
+			exit 0
+			;;
+		\?)
+			echo Invalid option!
+			echo
+			help
 			exit 0
 			;;
 	esac

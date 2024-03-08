72f5a020883e6e2d11d531b284b45a16e8317b69
Harry Han Ubuntu <s2162783@ed.ac.uk>
2024-03-08 16:22:56 +0000

    ud

--- config/bashrc.d/aliases.sh
+++ config/bashrc.d/aliases.sh
@@ -13,6 +13,23 @@ mkcd () {
 
 alias git-init="git init; echo -e '*.out\n*.o\n*.txt' >> .gitignore"
 
-alias ls=exa
+alias ls="exa"
 alias ll="exa -stime -l"
 alias cwall="bash ~/.bashrc.cmd.d/changeWallpaper.sh"
+alias v="nvim"
+alias pyvenv="source .venv/bin/activate"
+alias py="python3"
+
+
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+
---
+++ config/bashrc.d/path.sh
@@ -0,0 +1,3 @@
+export PATH=/usr/local/cuda-12.3/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-12.3/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PATH="$PATH:$HOME/.cargo/bin/"
